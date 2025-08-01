c0eb1e7af08780550f015864ecd7ce619eb6b2d2
Abdimahad1 <mucjisoduusho123@gmail.com>
2025-08-01 18:54:49 +0300

    last update

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+// ✅ ADD THIS LINE
+const Payment = require('../models/Payment');
+
 const authMiddleware = require('../middlewares/authMiddleware');
 const {
   processPayment,
